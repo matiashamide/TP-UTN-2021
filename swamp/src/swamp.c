139ca21a3a20d5d1bef7cd911190b52f98a2a97d
GuilleKalniker <guillermokal@hotmail.com>
2021-09-15 16:36:59 -0300

    subo proyectos de los 4 modulos con las commons linkeadas

---
+++ swamp/src/swamp.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : swamp.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<commons/string.h>
+
+int main(void) {
+	char* string = string_new();
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
