f82e81681c7673e3c8b757771e78793f7f4832a4
GuilleKalniker <guillermokal@hotmail.com>
2021-09-15 19:01:09 -0300

    corrijo errores

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -13,6 +13,15 @@
 int main(void) {
 
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	t_memoria_config config = crear_archivo_config_memoria("/home/utnso/workspace/tp-2021-2c-DesacatadOS/memoria/src/memoria.config");
+
+	printf("el puerto es: %d \n", config.puerto);
+	printf("el ip es: %s\n", config.ip);
+	printf("el alg de remplazo es: %s\n", config.alg_reemplazo_tlb);
+
+
+
 	return EXIT_SUCCESS;
 }
 
@@ -36,7 +45,7 @@ t_memoria_config crear_archivo_config_memoria(char* ruta) {
     config.tamanio = config_get_int_value(memoria_config, "TAMANIO");
     config.alg_remp_mmu = config_get_string_value(memoria_config, "ALGORITMO_REEMPLAZO_MMU");
     config.tipo_asignacion = config_get_string_value(memoria_config, "TIPO_ASIGNACION");
-    config.marcos_max = config_get_int_value(memoria_config, "MARCOS_MAXIMOS");
+    //config.marcos_max = config_get_int_value(memoria_config, "MARCOS_MAXIMOS");  --> ver en que casos esta esta esta config y meter un if
     config.cant_entradas_tlb = config_get_int_value(memoria_config, "CANTIDAD_ENTRADAS_TLB");
     config.alg_reemplazo_tlb = config_get_string_value(memoria_config, "ALGORITMO_REEMPLAZO_TLB");
     config.retardo_acierto_tlb = config_get_int_value(memoria_config, "RETARDO_ACIERTO_TLB");
