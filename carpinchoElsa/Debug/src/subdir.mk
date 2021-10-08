4d0922ab158d73a37c98437c8f6dc824fd2ded1d
utnso <sofia.cortez1509@gmail.com>
2021-10-08 19:26:37 -0300

    Cambio el nombre de carpinchos a carpinchoElsa

--- carpinchoElsa/Debug/src/subdir.mk
+++ carpinchoElsa/Debug/src/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/carpinchos.c 
+../src/carpinchoElsa.c 
 
 OBJS += \
-./src/carpinchos.o 
+./src/carpinchoElsa.o 
 
 C_DEPS += \
-./src/carpinchos.d 
+./src/carpinchoElsa.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
